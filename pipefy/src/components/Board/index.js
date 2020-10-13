2f638aa988a523ed68f9f700a2ec2eb08442c3dd
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-13 19:29:01 -0300

    add context and update Board component

--- pipefy/src/components/Board/index.js
+++ pipefy/src/components/Board/index.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import produce from 'immer'
 
+import BoardContext from './context'
 import List from '../List'
 
 import { Container } from './style'
 
 import { loadLists } from '../../services/api'
-const list = loadLists()
+const data = loadLists()
 
 export default function Board(){
+  const [lists, setLists] = useState(data)
+
+  function move(fromList, toList, from, to){
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
+  }
+
   return (
-    <Container>
-      {list.map(list => <List key={list.title} data={list}/> )}
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => <List key={list.title} listIndex={index} data={list}/> )}
+      </Container>
+    </BoardContext.Provider>
   )
 }
