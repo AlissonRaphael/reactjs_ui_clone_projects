c374eda0a9d88d3d1bb9968e4c5bc0152fc90184
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-13 12:04:50 -0300

    update Board component

--- pipefy/src/components/Board/index.js
+++ pipefy/src/components/Board/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import List from '../List'
+
 import { Container } from './style'
+
+import { loadLists } from '../../services/api'
+const list = loadLists()
 
 export default function Board(){
   return (
     <Container>
-      
+      {list.map(list => <List key={list.title} data={list}/> )}
     </Container>
   )
 }
