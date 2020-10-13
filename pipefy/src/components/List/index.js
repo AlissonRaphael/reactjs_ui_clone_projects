2763e2ada3e6149d526d9c3265d8d81a9bd627b1
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-13 12:06:23 -0300

    update List component

--- pipefy/src/components/List/index.js
+++ pipefy/src/components/List/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { MdAdd } from 'react-icons/md'
 
-import { Ul } from './style'
+import { Container } from './style'
 
-export default function List(){
+import Card from '../Card'
+
+export default function List({ data }){
   return (
-    <Ul>
-      
-    </Ul>
+    <Container done={data.done}>
+      <header>
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#fff"/>
+          </button>
+        )}
+      </header>
+
+      <ul>
+        {data.cards.map(card => <Card key={card.id} data={card}/>)}
+      </ul>
+    </Container>
   )
 }
