875a4af52013f2c85d34c2ebb3c585324628ae3c
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-14 12:55:27 -0300

    update ServerButton and correct layout grid

--- discord/src/components/ServerButton/index.js
+++ discord/src/components/ServerButton/index.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 import { Button  } from './styles'
 
-export default function ServerButton(){
+export default function ServerButton({ selected, isHome, hasNotifications, mentions }){
   return (
-    <Button></Button>
+    <Button
+      className={selected ? 'active' : ''}
+      isHome={isHome}
+      hasNotifications={hasNotifications}
+      mentions={mentions}
+    >
+
+    </Button>
   )
 }
--- discord/src/components/ServerButton/styles.js
+++ discord/src/components/ServerButton/styles.js
@@ -1,5 +1,70 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
 
+  width: 48px;
+  height: 48px;
+  border: none;
+  border-radius: 50%;
+
+  background-color: ${(props) => props.isHome ? 'tomato' : 'var(--primary)'};
+
+  cursor: pointer;
+  position: relative;
+  margin-bottom: 8px;
+
+  img {
+    height: 24px;
+    width: 24px;
+  }
+
+  transition: all 0.2s;
+
+  &.active, &:hover {
+    border-radius: 16px;
+    background-color: ${(props) => props.isHome ? 'tomato' : 'var(--discord)'}
+  }
+
+  &::before {
+    content: '';
+    width: 9px;
+    height: 9px;
+
+    position: absolute;
+    left: -15px;
+    top: calc(50% - 4.5px);
+
+    background-color: var(--white);
+    border-radius: 50%;
+
+    display: ${(props) => props.hasNotifications ? 'inline' : 'none' };
+  }
+
+  &::after {
+    content: '${(props) => props.mentions && props.mentions }';
+    display: ${(props) => props.mentions ? 'flex' : 'none' };
+    background-color: var(--notification);
+
+    width: auto;
+    height: 16px;
+
+    position: absolute;
+    bottom: -4px;
+    right: -4px;
+
+    padding: 0px 3px;
+    border-radius: 9px;
+    border: 4px solid var(--quaternary);
+
+    align-items: center;
+    justify-content: center;
+
+    font-size: 11px;
+    font-weight: bold;
+    color: var(--white);
+  }
 `
