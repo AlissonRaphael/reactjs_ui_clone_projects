e8069d81ef233fa6fe46573abf6a875c714d9176
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-14 17:14:35 -0300

    create ServerName component

---
+++ discord/src/components/ServerName/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container, Title, ExpandIcon } from './styles'
+
+export default function ServerName(){
+  return (
+    <Container>
+      <Title>Servidor dos amigos</Title>
+    </Container>
+  )
+}
