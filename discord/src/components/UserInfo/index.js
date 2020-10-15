7973cb183895d0dacab75a68897c44c5ec796968
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-15 12:53:27 -0300

    update UserInfo component

--- discord/src/components/UserInfo/index.js
+++ discord/src/components/UserInfo/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-import { Container, Profile, Avatar, UserData, Icons, MicIcon, HeadphoneIcon, SettingsIcon } from './styles'
+import {
+  Container,
+  Profile,
+  Avatar,
+  UserData,
+  Icons,
+  MicIcon,
+  HeadphoneIcon,
+  SettingsIcon } from './styles'
 
 export default function UserInfo(){
   return (
