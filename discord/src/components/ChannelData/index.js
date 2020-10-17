3e9a0b4239607112ec214e9b9cff9e74204a1395
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-17 13:08:09 -0300

    update ChannelData component

--- discord/src/components/ChannelData/index.js
+++ discord/src/components/ChannelData/index.js
@@ -2,13 +2,94 @@ import React from 'react'
 
 import { Container, Messages } from './styles'
 
+import ChannelMessages, { Mention } from '../ChannelMessages'
+
 import InputMessage from '../InputMessage'
 
 export default function(){
   return (
     <Container>
-      <Messages />
+
+      <Messages>
+        <ChannelMessages 
+          author='Alisson Oliveira'
+          date='25/06/2020'
+          content="tô montando o squad"
+          color='steelblue'
+        />
+
+        <ChannelMessages 
+          author='Alisson Oliveira'
+          date='25/06/2020'
+          content="falta dois"
+          color='steelblue'
+        />
+
+        <ChannelMessages 
+          author='C3P0'
+          date='25/06/2020'
+          content="\o/"
+          isBot
+        />
+
+        <ChannelMessages 
+          author='Alisson Oliveira'
+          date='25/06/2020'
+          content="entrem ai"
+          color='steelblue'
+        />
+
+        <ChannelMessages 
+          author='Alisson Oliveira'
+          date='25/06/2020'
+          content="go go go"
+          color='steelblue'
+        />
+
+        <ChannelMessages 
+          author='Talessa Marina'
+          date='25/06/2020'
+          content={
+            <>
+              <Mention>@Alisson Oliveira</Mention> eu vou
+            </>
+          }
+          hasMention
+          color='hotPink'
+        />
+
+        <ChannelMessages 
+          author='Talessa Marina'
+          date='25/06/2020'
+          content='coloque eu'
+          color='hotPink'
+        />
+
+        <ChannelMessages 
+          author='C3P0'
+          date='25/06/2020'
+          content="go!"
+          isBot
+        />
+
+        <ChannelMessages 
+          author='Alisson Oliveira'
+          date='25/06/2020'
+          content="entre ai, tô chamando..."
+          color='steelblue'
+        />
+
+        <ChannelMessages 
+          author='Talessa Marina'
+          date='25/06/2020'
+          content='to entrando'
+          color='hotPink'
+        />
+
+      </Messages>
+
       <InputMessage />
+
     </Container>
   )
 }
--- discord/src/components/ChannelData/styles.js
+++ discord/src/components/ChannelData/styles.js
@@ -7,26 +7,28 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   background-color: var(--primary);
-
+  max-height: calc(100vh - 46px);
 `
 
 export const Messages = styled.div`
   padding: 20px 0px;
   display: flex;
   flex-direction: column;
-  max-height: calc(100vh -46px -68px);
+  max-height: calc(100vh - 68px);
   overflow-y: scroll;
 
   ::-webkit-scrollbar {
-    width: 8px;
+    width: 12px;
   }
 
   ::-webkit-scrollbar-thumb {
-    background-color: var(--tertiary);
-    border-radius: 4px;
+    background-color: var(--secondary);
+    border: 4px solid var(--primary);
+    border-left: none;
+    border-radius: 6px;
   }
 
   ::-webkit-scrollbar-track {
-    background-color: var(--secondary);
+    background-color: var(--primary);
   }
 `
