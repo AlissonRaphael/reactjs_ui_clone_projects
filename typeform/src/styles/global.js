6c656c220170e52b860165888b6633c70e0e4b29
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-18 20:28:56 -0300

    add styled-components and global styles

---
+++ typeform/src/styles/global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    outline: none;
+  }
+`
