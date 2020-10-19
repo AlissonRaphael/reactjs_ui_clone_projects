2eb9632c0f8f47cb6dfbc42ae40eb31e6ee27c53
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-19 14:05:01 -0300

    update App and dependencies

--- typeform/src/App.js
+++ typeform/src/App.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import GlobalStyles from './styles/global'
 import { Main, Section, Sticky } from './styles/'
 
+import Header from './components/Header'
+import FirstAndSecond from './components/FirstAndSecond'
+import Footer from './components/Footer'
+
 export default function App(){
+  useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [])
 
   return (
     <>
-      <GlobalStyles/>
+      <GlobalStyles />
+
       <Main style={{ height: '1610vh' }}>
+        <Header />
+
         <Section style={{ height: '72.7%' }}>
-          <Sticky className="first"/>
-          <Sticky className="second"/>
+          <FirstAndSecond />
         </Section>
 
         <Section style={{ height: '9.7%' }}>
-          <Sticky className="third"/>
+          <Sticky className="third" />
         </Section>
 
         <Section style={{ height: '10.1%' }}>
-          <Sticky className="fourth"/>
+          <Sticky className="fourth" />
         </Section>
 
+        <Footer />
       </Main>
     </>
   )
