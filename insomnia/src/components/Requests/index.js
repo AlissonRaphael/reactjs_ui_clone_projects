5299a8792d99e0341caa6d0294a8bed21beaaaff
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-21 13:46:31 -0300

    update Requests component

--- insomnia/src/components/Requests/index.js
+++ insomnia/src/components/Requests/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 import { Container } from './style'
 
+import RequestOptions from '../RequestOptions'
+
 export default function Resquest(){
   return (
     <Container>
-      
+      <RequestOptions />
     </Container>
   )
 }
