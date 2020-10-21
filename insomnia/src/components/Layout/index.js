a4c6db658c6ee94aa33f6e586e8b10b99c4daeba
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-20 22:25:20 -0300

    create Layout component

---
+++ insomnia/src/components/Layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+import ActionList from '../ActionsList'
+
+export default function Layout(){
+  return (
+    <Container>
+      <ActionList/>
+      <div>2</div>
+
+    </Container>
+  )
+}
---
+++ insomnia/src/components/Layout/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  display: grid;
+  grid-template-columns: 22% 78%;
+`
