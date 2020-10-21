6413a809e822417a15282ff090bbd4814c54d3d5
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-21 18:23:49 -0300

    update components

--- insomnia/src/components/RequestConfig/index.js
+++ insomnia/src/components/RequestConfig/index.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Container, Menu } from './styles'
 import TabButton from '../TabButton'
+import Body from '../../pages/Body'
+import Auth from '../../pages/Auth'
+import Query from '../../pages/Query'
+import Header from '../../pages/Header'
+import Docs from '../../pages/Docs'
 
 export default function RequestConfig(){
   return (
@@ -13,6 +19,16 @@ export default function RequestConfig(){
         <TabButton>Header</TabButton>
         <TabButton>Docs</TabButton>
       </Menu>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/body' element={<Body/>}/>
+          <Route path='/auth' element={<Auth/>}/>
+          <Route path='/query' element={<Query/>}/>
+          <Route path='/header' element={<Header/>}/>
+          <Route path='/docs' element={<Docs/>}/>
+        </Routes>
+      </BrowserRouter>
     </Container>
   )
 }
--- insomnia/src/components/RequestOptions/index.js
+++ insomnia/src/components/RequestOptions/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Container, FirstSection, SecondSection, Button, MoreIcon, Input } from './styles'
+import RequestConfig from '../RequestConfig'
 
 export default function RequestOptions(){
   return (
@@ -19,7 +20,7 @@ export default function RequestOptions(){
       </FirstSection>
 
       <SecondSection>
-
+        <RequestConfig/>
       </SecondSection>
     </Container>
   )
