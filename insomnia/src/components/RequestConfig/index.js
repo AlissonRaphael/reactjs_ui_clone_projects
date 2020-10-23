2df023f8084b827291ea9ec77ce3bd269095b8ee
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-22 21:40:39 -0300

    update Request component

--- insomnia/src/components/RequestConfig/index.js
+++ insomnia/src/components/RequestConfig/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { NavLink, BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { Container, Menu } from './styles'
-import TabButton from '../TabButton'
+import { Menu, MoreIcon } from './styles'
 import Body from '../../pages/Body'
 import Auth from '../../pages/Auth'
 import Query from '../../pages/Query'
@@ -11,24 +10,38 @@ import Docs from '../../pages/Docs'
 
 export default function RequestConfig(){
   return (
-    <Container>
+    <BrowserRouter>
       <Menu>
-        <TabButton more>Body</TabButton>
-        <TabButton more active>Auth</TabButton>
-        <TabButton>Query</TabButton>
-        <TabButton>Header</TabButton>
-        <TabButton>Docs</TabButton>
+        <NavLink to="/body">
+          Body
+          <MoreIcon/>
+        </NavLink>
+
+        <NavLink to="/auth">
+          Auth
+          <MoreIcon/>
+        </NavLink>
+
+        <NavLink to="/query">
+          Query
+        </NavLink>
+
+        <NavLink to="/header">
+          Header
+        </NavLink>
+
+        <NavLink to="/docs">
+          Docs
+        </NavLink>
       </Menu>
 
-      <BrowserRouter>
-        <Routes>
-          <Route path='/body' element={<Body/>}/>
-          <Route path='/auth' element={<Auth/>}/>
-          <Route path='/query' element={<Query/>}/>
-          <Route path='/header' element={<Header/>}/>
-          <Route path='/docs' element={<Docs/>}/>
-        </Routes>
-      </BrowserRouter>
-    </Container>
+      <Routes>
+        <Route path='/body' element={<Body/>}/>
+        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/query' element={<Query/>}/>
+        <Route path='/header' element={<Header/>}/>
+        <Route path='/docs' element={<Docs/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
--- insomnia/src/components/RequestConfig/styles.js
+++ insomnia/src/components/RequestConfig/styles.js
@@ -1,15 +1,13 @@
 import styled from 'styled-components'
+import { MdArrowDropDown } from 'react-icons/md'
 
-export const Container = styled.div`
-
-`
 
 export const Menu = styled.div`
-  width: 100%;
   display: flex;
+  width: 100%;
   position: relative;
-
-  ::after {
+  
+  &:after {
     content: '';
     width: 100%;
     height: 1px;
@@ -18,6 +16,61 @@ export const Menu = styled.div`
     position: absolute;
     bottom: 0px;
     left: 0px;
-    z-index: 0;
   }
+
+  a {
+    font-size: 14px;
+    padding: 9px 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid var(--border);
+
+    z-index: 1;
+    color: var(--input-color);
+    background-color: transparent;
+    transition: background-color 0s ease 0.1s;
+
+    &:focus {
+      background-color: rgba(255,255,255, 0.2);
+    }
+
+    &.active {
+      color: #fff;
+      position: relative;
+      border-bottom: 1px solid var(--background);
+
+      &::after {
+        content: '';
+        width: 1px;
+        height: 100%;
+        background-color: var(--border);
+        position: absolute;
+        top: 0;
+        right: 0;
+        z-index: 2;
+      }
+    }
+
+    &.active:not(:first-child) {
+      position: relative;
+      &::before {
+        content: '';
+        width: 1px;
+        height: 100%;
+        background-color: var(--border);
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 2;
+      }
+
+    }
+
+  }
+`
+
+export const MoreIcon = styled(MdArrowDropDown)`
+  width: 20px;
+  height: 20px;
 `
