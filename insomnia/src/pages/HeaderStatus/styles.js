833f5ad6eebe6df1eff5a6131b9225324556d7f6
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-23 21:26:18 -0300

    create HeaderStatus page

---
+++ insomnia/src/pages/HeaderStatus/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    margin-top: 30px;
+    font-size: 14px;
+    color: rgba(255,255,255,0.55);
+  }
+
+  img {
+    opacity: 0.15;
+  }
+
+`
