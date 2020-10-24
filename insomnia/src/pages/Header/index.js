d5c2bcb46c304adcf0e7a43f8262ab2830c76e7c
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-23 21:10:29 -0300

    update Header page

--- insomnia/src/pages/Header/index.js
+++ insomnia/src/pages/Header/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import { Container, FirstSection, SecondSection, SettingIcon, Input, Button } from './styles'
+
 export default function Header(){
   return (
-    <div>Header</div>
+    <Container>
+      <FirstSection>
+        <div>
+          <SettingIcon/>
+          <Input placeholder="New name"/>
+          <Input placeholder="New value"/>
+        </div>
+      </FirstSection>
+
+      <SecondSection>
+        <Button>Bulk Edit</Button>
+      </SecondSection>
+    </Container>
   )
 }
