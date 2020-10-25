a5106af83ef71ac8798f3cb46d8dd2c9d0d7e896
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-24 21:24:19 -0300

    update App/Context

--- insomnia/src/App.js
+++ insomnia/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import RequestContext from './RequestContext'
 
 import GlobalStyle from './styles/global'
 import Layout from './components/Layout'
 
 export default function App(){
+  const [path, setPath] = useState('')
+
 
   return (
-    <>
+    <RequestContext.Provider value={[path, setPath]}>
       <GlobalStyle/>
       <Layout/>
-    </>
+    </RequestContext.Provider>
   )
 }
