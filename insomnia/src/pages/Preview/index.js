0a5a0eb9ada101ae8f612618e2c55a22eb054c0a
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-25 12:17:03 -0300

    update Preview page

--- insomnia/src/pages/Preview/index.js
+++ insomnia/src/pages/Preview/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
+import ReactJson from 'react-json-view'
 
 import RequestContext from '../../RequestContext'
 
@@ -10,8 +11,8 @@ export default function Preview(){
 
   useEffect(() => {
     async function getFile(){
-      const response = await fetch(path);
-      const json = await response.json();
+      const response = await fetch('https://api.github.com/')
+      const json = await response.json()
 
       setFile(json)
     }
@@ -20,10 +21,20 @@ export default function Preview(){
   }, [path])
 
 
-
   return (
     <Container>
-      { JSON.stringify(file, null, 2) }
+      <ReactJson
+        src={file}
+        enableClipboard={false}
+        onEdit={false}
+        onAdd={false}
+        onDelete={false}
+        collapseStringsAfterLength={20}
+        theme="monokai"
+        style={{
+          backgroundColor: 'transparent'
+        }}
+      />
     </Container>
   )
 }
--- insomnia/src/pages/Preview/styles.js
+++ insomnia/src/pages/Preview/styles.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: 100%;
+  padding: 12px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+  justify-content: flex-start;
 
   p {
     margin-top: 30px;
