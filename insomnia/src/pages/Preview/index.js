3b661fba8c84f792f2a41a5417359a249126e0b5
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-24 21:25:54 -0300

    update Preview page with context

--- insomnia/src/pages/Preview/index.js
+++ insomnia/src/pages/Preview/index.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+
+import RequestContext from '../../RequestContext'
 
 import { Container } from './styles'
 
 export default function Preview(){
+  const [file, setFile] = useState('')
+  const [path,] = useContext(RequestContext)
+
+  useEffect(() => {
+    async function getFile(){
+      const response = await fetch(path);
+      const json = await response.json();
+
+      setFile(json)
+    }
+
+    getFile()
+  }, [path])
+
+
+
   return (
     <Container>
+      { JSON.stringify(file, null, 2) }
     </Container>
   )
 }
