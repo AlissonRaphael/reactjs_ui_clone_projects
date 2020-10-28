60ac2c92c3ff361dc620ad12c1e35b21bd69dea9
Alisson Raphael <alissonraphaeloliveira@gmail.com>
2020-10-28 20:43:28 -0300

    update RequestConfig component

--- insomnia/src/components/RequestConfig/styles.js
+++ insomnia/src/components/RequestConfig/styles.js
@@ -54,10 +54,7 @@ export const Menu = styled.div`
         right: 0;
         z-index: 2;
       }
-    }
 
-    &.active:not(:first-child) {
-      position: relative;
       &::before {
         content: '';
         width: 1px;
@@ -68,9 +65,16 @@ export const Menu = styled.div`
         left: 0;
         z-index: 2;
       }
-
     }
+  }
 
+  div:first-child a.active {
+    position: relative;
+      &::before {
+        content: '';
+        width: 0;
+        height: 0;
+      }
   }
 `
 
